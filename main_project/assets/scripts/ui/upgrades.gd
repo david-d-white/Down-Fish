3d989ab6289b5a06f78759049ff85d12a9c79834
David White <david.daniel.white@gmail.com>
2023-08-14 22:51:38 -0400

    Main Menu Overhaul

--- main_project/assets/scripts/ui/upgrades.gd
+++ main_project/assets/scripts/ui/upgrades.gd
@@ -2,18 +2,28 @@ extends NinePatchRect
 
 #@onready var global_node = get_node("/root/GlobalVars")
 
+var is_hidden = false
+
+@onready var money_counter = $Current_money
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Globals.game_state != Globals.GAME_STATE.WAVE_COOLDOWN:
 		visible = false
 		return
 	var money = Globals.money
-	$Current_money.text = "$:" + str(money)
-	$Current_money.size.x = len($Current_money.text)*5
+	
+	money_counter.text = "$" + str(money)
+	money_counter.size.x = len(money_counter.text)*5
 	if abs(get_tree().get_root().get_node("main/Island").global_position.x - get_tree().get_root().get_node("main/boat").global_position.x) < 15:
-		visible = true
+		if is_hidden:
+			$AnimationPlayer.play("menu_open")
+			is_hidden = false
+			visible = true
 	else:
-		visible = false
+		if not is_hidden:
+			$AnimationPlayer.play("menu_close")
+			is_hidden = true
 
 
 func _on_base_upgrade_property_update(node_name, property_name, value):
